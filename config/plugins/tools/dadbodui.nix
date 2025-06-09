f580ded51722f34956ed1e53aea24500dbac6f4f
zepzeper <wouterschiedam98@gmail.com>
2025-06-09 13:16:44 +0200

    dadbod

---
+++ config/plugins/tools/dadbodui.nix
@@ -0,0 +1,34 @@
+{
+	plugins.vim-dadbod-completion.enable = true;
+	plugins.vim-dadbod.enable = true;
+	plugins.vim-dadbod-ui = {
+		enable = true;
+	};
+
+  keymaps = [
+    {
+      mode = "n";
+      key = "<leader>db";
+      action = ":DBUIToggle<CR>";
+      options.desc = "Toggle database UI";
+    }
+    {
+      mode = "n";
+      key = "<leader>df";
+      action = ":DBUIFindBuffer<CR>";
+      options.desc = "Find database buffer";
+    }
+    {
+      mode = "n";
+      key = "<leader>dr";
+      action = ":DBUIRenameBuffer<CR>";
+      options.desc = "Rename database buffer";
+    }
+    {
+      mode = "n";
+      key = "<leader>dl";
+      action = ":DBUILastQueryInfo<CR>";
+      options.desc = "Last query info";
+    }
+  ];
+}
